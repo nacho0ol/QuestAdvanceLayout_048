4e9c9a70a34dbb92d1832e0e0f976cb243bf84cf
nachool <nachma.olivia1394@gmail.com>
2025-10-24 15:50:27 +0700

    memanggilfunction di  mainactivity

--- app/src/main/java/com/example/questadvancelayout_048/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_048/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_048Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
