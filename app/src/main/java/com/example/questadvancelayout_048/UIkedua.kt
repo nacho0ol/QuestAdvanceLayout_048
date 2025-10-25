3af1fcc92c884e6fcaf28a388bb333ccf6ffb01e
nachool <nachma.olivia1394@gmail.com>
2025-10-25 18:57:41 +0700

    menambah row & mengisi row dengan image ig, tiktok, wa, dan email

--- app/src/main/java/com/example/questadvancelayout_048/UIkedua.kt
+++ app/src/main/java/com/example/questadvancelayout_048/UIkedua.kt
@@ -1,15 +1,20 @@
 package com.example.questadvancelayout_048
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,6 +47,21 @@ fun WarkopAnabul(modifier: Modifier){
 
         Spacer(modifier = Modifier.height(height = 24.dp))
 
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(painterResource(id = R.drawable.instagram), contentDescription = null, modifier = Modifier.size(40.dp))
+            Spacer(modifier = Modifier.width(width = 20.dp))
+            Image(painterResource(id = R.drawable.tiktok), contentDescription = null, modifier = Modifier.size(40.dp))
+            Spacer(modifier = Modifier.width(width = 20.dp))
+            Image(painterResource(id = R.drawable.whatsapp), contentDescription = null, modifier = Modifier.size(40.dp))
+            Spacer(modifier = Modifier.width(width = 20.dp))
+            Image(painterResource(id = R.drawable.email), contentDescription = null, modifier = Modifier.size(40.dp))
+
+
+        }
 
 
 
