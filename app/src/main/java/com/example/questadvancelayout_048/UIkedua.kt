040555f15d6022380547624f2778f427f5b2225f
nachool <nachma.olivia1394@gmail.com>
2025-10-25 18:51:28 +0700

    membuat spacer untuk column dan logo serta logo & row

--- app/src/main/java/com/example/questadvancelayout_048/UIkedua.kt
+++ app/src/main/java/com/example/questadvancelayout_048/UIkedua.kt
@@ -1,19 +1,49 @@
 package com.example.questadvancelayout_048
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import java.lang.reflect.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WarkopAnabul(modifier: Modifier){
-    Column(modifier = Modifier
-        .padding(top=50.dp)
-        .fillMaxSize(),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Spacer(modifier= Modifier.height(height = 40.dp))
+
+        val logo_company = painterResource(id = R.drawable.ellipse_48)
+        Image(
+            painter = logo_company,
+            contentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.height(height = 24.dp))
+
+
+
 
     }
 }
