2f66039857f648985adf93c0b2312f63ec97f0d3
nachool <nachma.olivia1394@gmail.com>
2025-10-25 17:59:41 +0700

    membuat function WarkopAnabul dan menambah column

--- app/src/main/java/com/example/questadvancelayout_048/UIkedua.kt
+++ app/src/main/java/com/example/questadvancelayout_048/UIkedua.kt
@@ -1,4 +1,19 @@
 package com.example.questadvancelayout_048
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import java.lang.reflect.Modifier
 
+@Composable
+fun WarkopAnabul(modifier: Modifier){
+    Column(modifier = Modifier
+        .padding(top=50.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+}
